4e9506ace49c8506f307032ce827d37494fce326
gonzalez <jsgonzalez15@cloud-ing2193.uniandes.edu.co>
2020-09-24 22:17:16 -0500

    senseplot almost works, we need to fix Float32 operations

--- scripts/sensor_plot.py
+++ scripts/sensor_plot.py
@@ -1,65 +1,77 @@
-#!/usr/bin/env python3
-import rospy
-import sys
-from std_msgs.msg import String
+#!/usr/bin/env python
+import rospy 
+import sys 
+from std_msgs.msg import String,Float32, Float32MultiArray
 from geometry_msgs.msg import Twist
-# from taller2_9 import
-from pynput import keyboard
+import numpy as np
 
-global ruta
-global archivo
-global estado
-global name
-global velocidad
-global linealX, linealY, linealZ, angularX, angularY, angularZ
+global ruta,archivo,estado,name
+global nVel,rightVel,leftVel,tiempo,n,simTime, initialTime
+n=0
+initialTime=0
+nVel=0
+rightVel=Float32(0	)
+leftVel=Float32(0)
+tiempo=0
+simTime=0
+global linealX,linealY,linealZ,angularX,angularY,angularZ
 
-estado = True
-name = ""
-
-linealX = []
-linealY = []
-linealZ = []
-angularX = []
-angularY = []
-angularZ = []
-
-# 2do punto
+ruta= "/home/jsgonzalez15/robotica_ws/src/taller2_9/resources/"
+archivo=""
+estado=True
+name=""
 
 
-def callback(data):
-	global linealX, linealY, linealZ, angularX, angularY, angularZ
-	linealX.append(data.linear.x)
-	linealY.append(data.linear.y)
-	linealZ.append(data.linear.z)
-	angularX.append(data.angular.x)
-	angularY.append(data.angular.y)
-	angularZ.append(data.angular.z)
+linealX=[]
+linealY=[]
+linealZ=[]
+angularX=[]
+angularY=[]
+angularZ=[]
 
-# def on_press(key):
+#2do punto
+def giveMeTime(data):
+	global simTime,initialTime
+	simTime=data
+	if initialTime==0:
+		initialTime=simTime
 
-# def on_release(key):
+#def on_press(key):
+    
+
+#def on_release(key):
+    
+def lectura(texto,n):
+	global nVel,rightVel,leftVel,tiempo
+	nVel=texto[0,0]
+	if n<nVel:
+		rightVel=Float32(texto[n,0])
+		leftVel=Float32(texto[n,1])
+		tiempo=texto[n,2]
+	else:
+		rightVel=0
+		leftVel=0
 
 def nodo():
-	rospy.init_node("sensor_plot", anonymous=True)
-    args = sys.argv[1]
-    ruta= "/home/robotica/catkin_ws/src/taller2_9/resources/"
-    archivo=np.loadtxt(ruta+args)
-    print(archivo)
-	# rospy.Subscriber("turtlebot_position",Twist,callback)	
-	# pub=rospy.Publisher('turtlebot_wheelsVel.',Twist,queue_size=10) #Conecta con el tópico turtlebot_cmdVel
+	global nVel,rightVel,leftVel,tiempo,simTime,initialTime,n 
+	rospy.init_node("sensor_plot", anonymous=True) #Inicializo el nodo
+	arg= sys.argv[1]
+	f=open(ruta + arg[:])
+	comandVel=np.loadtxt(f)
+
+	rospy.Subscriber("simulationTime",Float32,giveMeTime)	
+	pub=rospy.Publisher('turtlebot_wheelsVel',Float32MultiArray,queue_size=10) #Conecta con el topico turtlebot_cmdVel
 	rate=rospy.Rate(10) #Cantidad de datos por segundo que va a publicar
 	rospy.spin()
-	
 
-	# variable=Twist() #Variable de tipo "Twist" >> 1er vector linear, 2do vector ángular. Cada uno con {x,y,z}
-	# hilo1=keyboard.Listener(on_press=on_press,on_release=on_release) #Habilitando hilo para ejecutar las funciones en paralelo
-    # hilo1.start()
+	if simTime-initialTime>=Float32(tiempo):
+		n=n+1
+		initialTime=simTime
+		lectura(comandVel,n)
+	
+	variable=Float32MultiArray(leftVel,rightVel) #Variable de tipo "Twist" >> 1er vector linear, 2do vector angular. Cada uno con {x,y,z}
+	pub.publish(variable)
 
 
 if __name__ == '__main__':
-	argv=
-	args =rospy.myargv(argv==sys.argv)
 	nodo()
-	f=open(ruta + args[1])
-	name=readlines(f)
-	print(name)
